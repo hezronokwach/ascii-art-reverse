d4f36711df36aecd15490a700b9fb6e0dc872024
danyonyi <denilanyonyi1@gmail.com>
2024-08-20 13:58:34 +0300

    refactor: move readfile.go and slice.go from functions to reverse folder for easier maintainability

--- reverse/slice.go
+++ reverse/slice.go
@@ -1,6 +1,6 @@
-package functions
+package reverse
 
-func SliceFile(fileSlice []string) [][]striing {
+func SliceFile(fileSlice []string) [][]string {
 	var result [][]string
 	for i := 0; i < len(fileSlice)-1; i += 9 {
 		file := fileSlice[i+1 : i+9]
